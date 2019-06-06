579716558e77c3924915e88f458e3bbe0a41a0b0
Jose-Bustamante <jose.bustamante@eriks.nl>
2019-06-06 15:48:31 +0200

    Test2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { withAuthenticator } from 'aws-amplify-react'
+import { Auth } from 'aws-amplify'
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,27 +22,37 @@ function UpdateMeetup() {
   return <h2>Update a meetup</h2>;
 }
 
-function App() {
-  return (
-    <Router>
-      <div>
-        MENU:
-        <nav>
-          <ul>
-            <li><Link to="/">All meetups</Link></li>
-            <li><Link to="/123">Single meetup</Link></li>
-            <li><Link to="/add">Add a meetup</Link></li>
-            <li><Link to="/update/123">Update a meetup</Link></li>
-          </ul>
-        </nav>
 
-        <Route path="/" exact component={Meetups} />
-        <Route path="/meetups/:id" component={SingleMeetup} />
-        <Route path="/add" component={AddMeetup} />
-        <Route path="/update/:id" component={UpdateMeetup} />
-      </div>
-    </Router>
-  );
+
+class App extends React.Component {
+
+  async componentDidMount() {
+    const user = await Auth.currentAuthenticatedUser()
+    console.log('user:', user)
+  }
+
+  render () {
+    return (
+      <Router>
+        <div>
+          MENU
+          <nav>
+            <ul>
+              <li><Link to="/">All meetups</Link></li>
+              <li><Link to="/123">Single meetup</Link></li>
+              <li><Link to="/add">Add a meetup</Link></li>
+              <li><Link to="/update/123">Update a meetup</Link></li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Meetups} />
+          <Route path="/meetups/:id" component={SingleMeetup} />
+          <Route path="/add" component={AddMeetup} />
+          <Route path="/update/:id" component={UpdateMeetup} />
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true })
